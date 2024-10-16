1ffecb5fe228dce968d11f9935eef1c4b6cfef3f
Ayush Sahu <ayushsahu007ay@gmail.com>
2024-10-16 16:43:31 +0530

    1st repo

---
+++ Flower.java
@@ -0,0 +1,10 @@
+class Flower{
+   String name  = " Rose";
+   String color = "Red"; 
+	public static void main(String[] args){
+	Flower f = new Flower();
+	System.out.println("Flower Name - " + f.name);
+	System.out.println("Flower color -" + f.color);
+
+	}
+}
